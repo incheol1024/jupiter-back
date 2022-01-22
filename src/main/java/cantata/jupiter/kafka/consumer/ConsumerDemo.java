593250785bdde3c6601b6e64fee691ca1cd7957a
incheol <yaho1024@naver.com>
2022-01-22 12:23:41 +0900

    update build.gradle add KafkaConfig

---
+++ src/main/java/cantata/jupiter/kafka/consumer/ConsumerDemo.java
@@ -0,0 +1,7 @@
+package cantata.jupiter.kafka.consumer;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConsumerDemo {
+}
---
+++ src/main/java/cantata/jupiter/kafka/producer/ProducerDemo.java
@@ -0,0 +1,7 @@
+package cantata.jupiter.kafka.producer;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProducerDemo {
+}
